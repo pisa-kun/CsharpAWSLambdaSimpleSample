b6b09a412aa3130e9c3e144ae4fc1e46c01046b6
pisa-kun <pisa0401mst@gmail.com>
2023-08-20 00:47:34 +0900

    "input" type change from str to obj

--- AWSLambdaSimpleSample/Function.cs
+++ AWSLambdaSimpleSample/Function.cs
@@ -1,6 +1,7 @@
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
 using System.Net;
+using System.Text.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -9,18 +10,18 @@ namespace AWSLambdaSimpleSample;
 
 public class Function
 {
-    
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
-    /// <param name="input"></param>
+    /// <param name="input">https://stackoverflow.com/questions/50322362/pass-json-string-to-aws-lambda-in-step-function-jsonreaderexception-error</param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public LambdaResponse FunctionHandler(string input, ILambdaContext context)
+    public LambdaResponse FunctionHandler(object input, ILambdaContext context)
     {
         var body = new Dictionary<string, string>()
         {
-            {"message", "Hello World!" },
+            {"message", "Hello, World!" },
         };
 
         var response = new LambdaResponse()
@@ -42,15 +43,15 @@ public class Function
 /// </summary>
 public class LambdaResponse
 {
-    [JsonProperty(PropertyName = "isBase64Encoded")] 
+    [JsonProperty(PropertyName = "isBase64Encoded")]
     public bool isBase64Encoded;
 
-    [JsonProperty(PropertyName = "statusCode")] 
+    [JsonProperty(PropertyName = "statusCode")]
     public HttpStatusCode statusCode;
 
     [JsonProperty(PropertyName = "headers")]
-    public Dictionary<string, string> headers;
+    public Dictionary<string, string>? headers;
 
     [JsonProperty(PropertyName = "body")]
-    public string body;
+    public string? body;
 }
